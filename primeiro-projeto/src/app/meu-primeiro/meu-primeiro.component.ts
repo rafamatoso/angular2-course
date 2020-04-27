dde2ac8b38a96c923cc767dcdb947f396cb8d975
Rafael Matoso <rafaelmatosog@gmail.com>
2020-04-26 22:25:19 -0300

    Project created

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `<span>Meu primeiro component com Angular 2!</span>`,
+})
+export class MeuPrimeiroComponent {}
